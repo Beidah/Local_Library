1eb44a777d9356f01b7c8e85eba6279cd08f0b00
Hunter Marshall <beidah@gmail.com>
2021-01-15 14:05:58 -0600

    Finish accounts page

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,37 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  return accounts.find(account => account.id === id);
+}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  return accounts.sort((a, b) => a.name.last.toLowerCase() > b.name.last.toLowerCase() ? 1 : -1);
+}
 
-function numberOfBorrows(account, books) {}
+function numberOfBorrows(account, books) {
+  let id = account.id;
+  // Double accumulate!
+  let borrows = books.reduce((acc, book) => {
+    return acc + book.borrows.reduce((acc, borrow) => {
+      if (borrow.id === id) {
+        acc += 1;
+      }
+      return acc;
+    }, 0);
+  }, 0);
+  return borrows;
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function getBooksPossessedByAccount(account, books, authors) {
+  console.log("Account id: " + account.id);
+  const authorIds = authors.map(author => author.id);
+  let booksByAuthors = books.filter(book => authorIds.includes(book.authorId)).map(book => {
+    book.author = findAccountById(authors, book.authorId);
+    return book;
+  });
+  let results = booksByAuthors.filter(book => {
+    return book.borrows.some(borrow => borrow.id === account.id && !borrow.returned);
+  });
+  return results;
+}
 
 module.exports = {
   findAccountById,

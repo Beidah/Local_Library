5d97512771d352768f8f67441d26f4959df4e629
Hunter Marshall <beidah@gmail.com>
2021-01-15 14:33:06 -0600

    finish book page

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,36 @@
-function findAuthorById(authors, id) {}
+// Helper functions
 
-function findBookById(books, id) {}
+function bookIsBorrowed(book) {
+  return book.borrows.some(borrow => !borrow.returned);
+}
 
-function partitionBooksByBorrowedStatus(books) {}
 
-function getBorrowersForBook(book, accounts) {}
+// Tested functions
+
+function findAuthorById(authors, id) {
+  return authors.find(author => author.id === id);
+}
+
+function findBookById(books, id) {
+  return books.find(book => book.id === id);
+}
+
+function partitionBooksByBorrowedStatus(books) {
+  const borrowedBooks = books.filter(bookIsBorrowed);
+  console.log(borrowedBooks);
+  const returnedBooks = books.filter(book => !bookIsBorrowed(book));
+  console.log(returnedBooks);
+  return [borrowedBooks, returnedBooks];
+}
+
+function getBorrowersForBook(book, accounts) {
+  let accountsWithBorrows = accounts.filter(account => book.borrows.some(borrow => borrow.id === account.id));
+  return accountsWithBorrows.map(account => {
+    let borrow = book.borrows.find(borrow => borrow.id === account.id);
+    account.returned = borrow.returned;
+    return account;
+  })
+}
 
 module.exports = {
   findAuthorById,
